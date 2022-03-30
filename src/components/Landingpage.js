4b874b511bcb83a431e68be5682e6e5191e47274
Sid <sidchopp@gmail.com>
2022-03-30 08:51:24 -0600

    About me des modified

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -9,7 +9,7 @@ const Landingpage = () => {
           About Me
         </h1>
         <p>
-          I'm Sid. I was born in India and moved to Calgary AB,🍁 in 2019. As a true lover of Science and Technology, I love building and deploying cool Web apps that accomplish business goals (like this App is deployed using AWS Amplify) and have a genuine passion for Physics. In my free time, I love to travel & explore.
+          I'm Sid. I was born in India and moved to Calgary🍁 in 2019. As a true lover of Science and Technology, I love building and deploying cool Web apps that accomplish business goals (like this App is deployed using AWS Amplify) and have a genuine passion for Physics. In my free time, I love to travel & explore.
         </p>
         <a href='https://sid-projects.netlify.app/' target="_blank" rel="noreferrer">
           <button
